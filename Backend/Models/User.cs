27acfbb62cc1e531d41d622e3f01bba2384f0cb4
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2025-02-14 23:48:07 +0530

    update user model

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -6,11 +6,13 @@
         public String FirstName { get; set; }
         public String LastName { get; set; }
         public String Email { get; set; }
+        public String NormalizedEmail { get; set; }
         public String HashedPassword { get; set; }
-        public String NormalizedPassword { get; set; }
         public String Role { get; set; }
-        public DateTime CreatedAt { get; set; }
         public Boolean EmailVerified { get; set; }
         public Boolean IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime LastLogin { get; set; }
     }
 }
