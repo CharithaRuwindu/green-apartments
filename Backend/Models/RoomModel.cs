8ed78f17be594885c141bcf98d59568b695a628b
B.C.R.N.P.BANDARAADHIKARAM <92729642+CharithaRuwindu@users.noreply.github.com>
2025-02-26 23:54:52 +0530

    Room Model creation

---
+++ Backend/Models/RoomModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class RoomModel
+    {
+        [Required, MaxLength(2)]
+        public int RoomCode { get; set; }
+        [Required]
+        public Guid RoomCategoryId { get; set; }
+    }
+}
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef8e3df1d8c9c8573d37add2fb39d48ebf0c071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+584d25b6a79fde2b7a299f0db3728ac8ddce2dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
